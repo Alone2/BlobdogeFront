20235c0d6c1b8ff044101e1d9b04acaea897d0f3
Alone2 <alain.sinzig@gmail.com>
2020-05-01 02:38:30 +0200

    Each news article has a link now

--- WebContent/js/content.js
+++ WebContent/js/content.js
@@ -12,13 +12,19 @@ function getHome() {
     $.getJSON("content/homeNews", function (data) {
         for (i = 0; i < data.length; i++) {
             new_dat = data[i]
+            titlewithoutspaces = new_dat["header"].replace(/\s/g, '');
+            titlesimple = titlewithoutspaces.replace(/[^a-zA-Z ]/g, "");
             if (i == 0) {
             	var a = '<div id="BlobNews" class="write content" style="font-size:medium"><b>News</b></div>'
-            	var b =  '<div class="content"><small class="date">' + new_dat["date"] + '</small><img class="blobimage" src="' + new_dat["pic"] + '" alt=""><h3>' + new_dat["header"] + "</h3><br />" + new_dat["message"] + "<br /><br /></div>"
+            	var b =  '<div class="content" id="news-' + titlesimple + '"><small class="date">' + new_dat["date"] + '</small><img class="blobimage" src="' + new_dat["pic"] + '" alt=""><h3>' + new_dat["header"] + "</h3><br />" + new_dat["message"] + "<br /><br /></div>"
             	$("#blobs").html(a + b + $("#blobs").html());
             } else {
-            	$("#blobs").html($("#blobs").html() + '<div class="content"><small class="date">' + new_dat["date"] + '</small><img class="blobimage" src="' + new_dat["pic"] + '" alt=""><h3>' + new_dat["header"] + "</h3><br />" + new_dat["message"] + "<br /><br /></div>");
+            	$("#blobs").html($("#blobs").html() + '<div class="content" id="news-' + titlesimple + '"><small class="date">' + new_dat["date"] + '</small><img class="blobimage" src="' + new_dat["pic"] + '" alt=""><h3>' + new_dat["header"] + "</h3><br />" + new_dat["message"] + "<br /><br /></div>");
             }
+        }
+        // to see if link to it with #
+        if (location.href.split("#").length > 1) {
+        	location.href = location.href;
         }
     });
 }
